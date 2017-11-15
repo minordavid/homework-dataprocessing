08c744b22bba16dfc73c38058ab3dcde565cd27b
David Vesseur <davidsiboos@live.nl>
2017-11-15 13:16:15 +0000

    3

--- week3/barchart.js
+++ week3/barchart.js
@@ -1,79 +1,92 @@
+// d3 tip from "http://bl.ocks.org/Caged/6476579"
+// make bar from "http://bl.ocks.org/Jverma/887877fc5c2c2d99be10"
+
+
 window.onload = function(){
-// set the dimensions of the canvas
-var margin = {top: 20, right: 20, bottom: 70, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    // set the margins and the width and hight
+    var margin = {top: 40, right: 20, bottom: 100, left: 40},
+        width = 700 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // set the ranges
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+    var y = d3.scale.linear().range([height, 0]);
+
+    // define the axis
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
 
-// set the ranges
-var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10);
 
-var y = d3.scale.linear().range([height, 0]);
-
-// define the axis
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom")
-
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left")
-    .ticks(10);
+    var tip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-10, 0])
+      .html(function(d) {
+        return "<strong>Liter:</strong> <span style='color:red'>" + d.Liter + "</span>";
+      });
 
 
-// add the SVG element
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+    // add the SVG element
+    var svg = d3.select("body").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    svg.call(tip);
+
+    // load the data
+    d3.json("Beer.json", function(error, data) {
+
+        data.forEach(function(d) {
+            d.Country = d.Country;
+            d.Liter = +d.Liter;
+        });
+
+      // scale the range of the data
+      x.domain(data.map(function(d) { return d.Country; }));
+      y.domain([0, d3.max(data, function(d) { return d.Liter; })]);
+
+      // add axis
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis)
+        .selectAll("text")
+          .style("text-anchor", "end")
+          .attr("dx", "-.8em")
+          .attr("dy", "-.55em")
+          .attr("transform", "rotate(-90)" );
+
+    // make the y as
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 5)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Liter / inwoner");
 
 
-// load the data
-d3.json("Beer.json", function(error, data) {
-
-    data.forEach(function(d) {
-        d.Country = d.Country;
-        d.Liter = +d.Liter;
+      // Add bar chart
+      svg.selectAll("bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.Country); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.Liter); })
+          .attr("height", function(d) { return height - y(d.Liter);})
+          .on('mouseover', tip.show)
+          .on('mouseout', tip.hide);
     });
-
-  // scale the range of the data
-  x.domain(data.map(function(d) { return d.Country; }));
-  y.domain([0, d3.max(data, function(d) { return d.Liter; })]);
-
-  // add axis
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .selectAll("text")
-      .style("text-anchor", "end")
-      .attr("dx", "-.8em")
-      .attr("dy", "-.55em")
-      .attr("transform", "rotate(-90)" );
-
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 5)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Liter / resident");
-
-
-  // Add bar chart
-  svg.selectAll("bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.Country); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.Liter); })
-      .attr("height", function(d) { return height - y(d.Liter); });
-
-});
 };
